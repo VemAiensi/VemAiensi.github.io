87023e62635ca5fc327b4e181fe2ab613f0163e4
Marianne Edic <142379309+MedicMedic@users.noreply.github.com>
2023-11-27 00:16:48 +0800

    Program Complete!
    I left a message in the HTML

--- SemanticsProject/basicFunctions.js
+++ SemanticsProject/basicFunctions.js
@@ -69,6 +69,9 @@ function clearTextArea()
     // Enable the "Open File" button
     openFileButton.disabled = false;
 
+    //Clear the file
+    document.getElementById('fileInput').value = '';
+
     // Disable all other buttons
     lexemeButton.disabled = true;
     syntaxButton.disabled = true;
